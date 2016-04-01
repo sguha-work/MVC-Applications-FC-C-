3f02ad402d7771a4d653a799c2b2610ed32b673b
sguha-work <sguha1988.life@gmail.com>
2016-04-01 14:04:50 +0530

    prepared json data from database

---
+++ MvcApplicationDataBase/MvcApplicationDataBase/Controllers/ChartController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplicationDataBase.Models;
+
+namespace MvcApplicationDataBase.Controllers
+{
+    public class ChartController : Controller
+    {
+        //
+        // GET: /Chart/
+        private dynamic getDataFromModel() 
+        {
+            WorldPopulationEntities storeDB = new WorldPopulationEntities();
+            return storeDB.WorldPopulations.ToList();
+        }
+        public ActionResult Index()
+        {
+            var dataFromDatabase = getDataFromModel();
+            // preparing the json data for the chart
+            String jsonString = "[";
+            foreach (var country in dataFromDatabase)
+            {
+                String countryName = country.CountryName;
+                jsonString += "{\"name\": \"" + countryName + "\",\"population\": \"" + country.Population + "\"},";
+            }
+            jsonString = jsonString.Remove(jsonString.Length - 1);
+            jsonString += "]";
+
+            return View();
+        }
+
+    }
+}
---
+++ MvcApplicationDataBase/MvcApplicationDataBase/Views/Chart/Index.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <title>Chart</title>
+</head>
+<body>
+    <div>
+        hello
+    </div>
+</body>
+</html>
